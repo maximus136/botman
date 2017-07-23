577f78870910f3e103c9ab34a8fc09397421cd97
maximus136 <>
2017-07-23 23:42:39 +0530

    webpack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,6 +4,9 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
 var OfflinePlugin = require('offline-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
+const FailPlugin = require('webpack-fail-plugin');
+const autoprefixer = require('autoprefixer');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 
 const pkg = require('./package.json');
 
@@ -16,10 +19,33 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.[chunkhash:8].js',
   },
+  node: {
+    fs: 'empty'
+  },
+  resolve: {
+    alias: {
+      // directories must start with '/' to resolve correctly
+      altSrc: __dirname + '/src/alt',
+      helpers: __dirname + '/src/helpers'
+    }
+  },
   plugins: [
     new CleanWebpackPlugin(["dist"], { verbose: false }),
     new HtmlWebpackPlugin({
       template: 'public/index.html'
+    }),
+
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: () => [autoprefixer]
+      }
+    }),
+    new SWPrecacheWebpackPlugin({
+      cacheId: 'botman',
+      dontCacheBustUrlsMatching: /\.\w{8}\./,
+      filename: 'sw.js',
+      minify: true,
+      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/]
     }),
     new CopyWebpackPlugin([{ from: 'public/' }]),
     new webpack.optimize.CommonsChunkPlugin({
@@ -48,12 +74,28 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: 'babel-loader',
-      include: path.join(__dirname, 'src'),
+      loader: 'babel-loader',
+      include: path.join(__dirname, 'src')
     },
     {
-      test: /\.css/,
-      loaders: ["style-loader", "css-loader"]
-    }]
+      test: /\.(css|scss)$/,
+      loaders: [
+        'style-loader',
+        'css-loader',
+        'sass-loader',
+        'postcss-loader'
+      ]
+    },
+    {
+      test: /\.(eot|svg|ttf|woff|woff2)$/,
+      loaders: [
+        'file-loader'
+      ]
+    },
+    {
+      test: /\.(png|jpg|svg)$/,
+      loader: 'url-loader?limit=8192'
+    }
+    ]
   }
 };
